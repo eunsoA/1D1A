6fc8b6c2c4f05dbed1537199606c57736c8f839b
Oliy <74090200+eunsoA@users.noreply.github.com>
2024-11-19 20:30:17 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -5,47 +5,44 @@
 
 using namespace std;
 
-int dp(vector<vector<int>> &RGB, vector<vector<int>> &memo, int N, int pre, int nCount) {
-    if (nCount == N) { // 종료 조건: 모든 집을 칠했을 때
+int dp(vector<vector<int>> &RGB, vector<vector <int>> &memo, int N, int pre, int nCount){
+    if(nCount == N)
         return 0;
-    }
-
-    if (memo[pre][nCount] != -1) { // 메모이제이션: 이미 계산된 경우
+    
+    if(memo[pre][nCount] != -1)
         return memo[pre][nCount];
-    }
-
-    // 현재 색깔의 비용 계산
+    
     int count = RGB[pre][nCount];
     count += min(
-        dp(RGB, memo, N, (pre + 1) % 3, nCount + 1), // 다음 집은 다른 색으로 칠함
-        dp(RGB, memo, N, (pre + 2) % 3, nCount + 1)  // 다음 집은 다른 색으로 칠함
+        dp(RGB, memo, N, (pre+1)%3, nCount+1),
+        dp(RGB, memo, N, (pre+2)%3, nCount+1)
     );
-
-    memo[pre][nCount] = count; // 결과 저장
+    memo[pre][nCount] = count;
     return count;
 }
 
-int main() {
+int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     int N;
     cin >> N;
 
-    // RGB 비용 입력
-    vector<vector<int>> RGB(3, vector<int>(N));
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < 3; j++) {
+    // RGB input
+    vector<vector<int>> RGB(3, vector<int> (N));
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<3; j++){
             cin >> RGB[j][i];
         }
     }
 
-    // 메모이제이션 배열 초기화
-    vector<vector<int>> memo(3, vector<int>(N, -1));
+    // memo
+    vector<vector<int>> memo(3, vector<int> (N, -1));
 
-    // 최소 비용 계산
+    // 
     int mincount = INT_MAX;
-    for (int i = 0; i < 3; i++) {
+    for(int i=0; i<3; i++){
         mincount = min(mincount, dp(RGB, memo, N, i, 0));
     }
     cout << mincount << "\n";
