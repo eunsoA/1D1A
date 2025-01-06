c9cf9fdec5d12a15ac1dfb7402e53cf5df9dd580
Oliy <74090200+eunsoA@users.noreply.github.com>
2025-01-06 16:49:42 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,22 +1,34 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main() {
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int T;
     cin >> T;
 
-    // 최대 40까지의 피보나치 호출 횟수를 미리 계산하여 저장
-    int dp[41][2] = { {1, 0}, {0, 1} }; // dp[i][0] = fibonacci(i)에서 0의 호출 횟수, dp[i][1] = fibonacci(i)에서 1의 호출 횟수
+    vector<int> dp0(41, 0);
+    vector<int> dp1(41, 0);
 
-    for (int i = 2; i <= 40; ++i) {
-        dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
-        dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+    dp0[0] = 1;
+    dp1[0] = 0;
+    dp0[1] = 0;
+    dp1[1] = 1;
+    for(int i=2; i<=41; i++){
+        dp0[i] = dp0[i-1] + dp0[i-2];
+        dp1[i] = dp1[i-1] + dp1[i-2];
     }
 
-    while (T--) {
-        int n;
-        cin >> n;
-        cout << dp[n][0] << " " << dp[n][1] << endl;
+    while (T)
+    {
+        int N;
+        cin >> N;
+        cout << dp0[N] << " " << dp1[N] << "\n";
+        T--;
     }
 
     return 0;
