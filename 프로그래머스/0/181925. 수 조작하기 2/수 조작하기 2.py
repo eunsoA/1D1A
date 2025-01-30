e076cc99229741b41d1d8dfb91ffffab07fa8d24
Claire <74090200+eunsoA@users.noreply.github.com>
2025-01-30 13:50:21 +0900

    [level 0] Title: 수 조작하기 2, Time: 27.56 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,8 +1,6 @@
 def solution(numLog):
-    answer =''
-    for i in range(1,len(numLog)) :
-        if numLog[i]-numLog[i-1]==1 : answer += 'w'
-        elif numLog[i]-numLog[i-1]==-1 : answer += 's'
-        elif numLog[i]-numLog[i-1]==10 : answer += 'd'
-        else : answer += 'a'
+    answer = ''
+    key = dict(zip([1,-1,10,-10],['w','s','d','a']))
+    for i in range(1,len(numLog)):
+        answer += key[numLog[i]-numLog[i-1]]
     return answer
