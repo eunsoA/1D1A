379e68c01fe972e4c5e07fbeaa19c52edc7c14ba
Claire <74090200+eunsoA@users.noreply.github.com>
2025-02-01 01:49:58 +0900

    [level 0] Title: 배열 만들기 4, Time: 37.10 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -1,14 +1,7 @@
 def solution(arr) :
     stk = []
-    i = 0  # 인덱스 초기화
-    while i < len(arr):  # 배열 크기보다 작을 때까지 반복
-        if not stk :
-            stk.append(arr[i])
-            i+=1
-        else :
-            if stk[-1] < arr[i]:
-                stk.append(arr[i])
-                i += 1
-            else : 
-                stk.pop()            
+    for i in range(len(arr)) :
+        while stk and stk[-1]>=arr[i]:
+            stk.pop()
+        stk.append(arr[i])           
     return stk
