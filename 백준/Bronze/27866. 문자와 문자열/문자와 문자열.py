997a26905b29ddea9d57f3bd2db6d659274cc6d5
Claire <74090200+eunsoA@users.noreply.github.com>
2025-02-03 02:07:19 +0900

    [Bronze V] Title: 문자와 문자열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.py
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.py
@@ -1,6 +1,6 @@
 import sys
+data = sys.stdin.read().splitlines()
+string = data[0].strip()
+count = int(data[1])-1
 
-mystring = sys.stdin.readline().strip()
-count = sys.stdin.readline().strip()
-
-print(mystring[int(count)-1])
+print(string[count])
