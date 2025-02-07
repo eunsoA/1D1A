ca7424e5cdb2a0adb0e9af12fa31d50dbffb7a0a
Claire <74090200+eunsoA@users.noreply.github.com>
2025-02-07 15:20:41 +0900

    [level 0] Title: 2의 영역, Time: 1.85 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,7 +1,4 @@
 def solution(arr):
-    try :
-        min = arr.index(2, 0)
-        max = arr[::-1].index(2, 0)
-        return arr[min:len(arr)-max] if min != len(arr)-max else [arr[min]]
-    except :
+    if 2 not in arr:
         return [-1]
+    return arr[arr.index(2):len(arr) - arr[::-1].index(2)]
