566ed5d08ed738a159ec37e0d82f40252d28b24f
Oliy <eunsoa0310@gmail.com>
2025-02-08 20:11:05 +0900

    update 0208

---
+++ testPy/1543.py
@@ -0,0 +1,16 @@
+import sys
+
+docs = sys.stdin.readline().strip()
+word = sys.stdin.readline().strip()
+
+count = 0
+i=0
+while i <= len(docs)-len(word) :
+    if docs[i:i+len(word)] == word :
+        count += 1
+        i += len(word)
+    else:
+        i += 1
+
+print(count)
+# 제출못함
