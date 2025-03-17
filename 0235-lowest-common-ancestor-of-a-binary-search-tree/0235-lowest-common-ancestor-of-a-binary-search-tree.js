7c97861532e510cc1cd0fac3ac38e3fd4729ee0e
Claire <74090200+eunsoA@users.noreply.github.com>
2025-03-17 13:39:43 +0900

    Time: 63 ms (71.05%), Space: 65.4 MB (27.97%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -15,9 +15,9 @@
 var lowestCommonAncestor = function (root, p, q) {
   while (root) {
     if (root.val > p.val && root.val > q.val) {
-      return lowestCommonAncestor(root.left, p, q);
+      root = root.left;
     } else if (root.val < p.val && root.val < q.val) {
-      return lowestCommonAncestor(root.right, p, q);
+      root = root.right;
     } else {
       return root;
     }
