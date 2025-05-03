d3d7811c9202dad47df21907bb771cea9a79a99d
Claire <eunsoa0310@gmail.com>
2025-05-03 14:52:42 +0900

    [Silver I] Title: IOIOI, Time: 160 ms, Memory: 11852 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -5,17 +5,22 @@ const N = parseInt(input[0]);
 const M = parseInt(input[1]);
 const S = input[2];
 
-const PN = 'IO'.repeat(N) + 'I';
-
 let count = 0;
 let i = 0;
+let patternCount = 0;
 
-while (i < M) {
-  if (S.slice(i, i + PN.length) === PN) {
-    count++;
+while (i < M - 1) {
+  if (S[i] === 'I' && S[i + 1] === 'O' && S[i + 2] === 'I') {
+    patternCount++;
     i += 2;
+    if (patternCount === N) {
+      count++;
+      patternCount--;
+    }
   } else {
+    patternCount = 0;
     i++;
   }
 }
+
 console.log(count);
