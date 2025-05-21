7c50a1572ece1227e1d62e18956ccc9431eac5a1
Claire <eunsoa0310@gmail.com>
2025-05-21 13:38:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.37 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,17 +1,6 @@
 function solution(n, arr1, arr2) {
-    var answer = [];
-    for(let i = 0; i<n; i++){
-        arr1[i] = arr1[i].toString(2).padStart(n,0);
-        arr2[i] = arr2[i].toString(2).padStart(n,0);
-        let line = '';
-        for(let j = 0; j<n; j++){
-            if(arr1[i][j] === '0' && arr2[i][j] === '0'){
-                 line+=' ';
-            }else{
-                line += '#';
-            }
-        }
-        answer.push(line);
-    }
-    return answer;
+    return arr1.map((num1, i)=>{
+        const orResult = (num1|arr2[i]).toString(2).padStart(n, '0');
+        return orResult.replace(/./g , ch => ch === '1' ? '#' : ' ');
+    });
 }
