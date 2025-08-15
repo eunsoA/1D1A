d1d48abddf4f52fe54d4485995ff4d7840812349
Eunso Ahn <eunsoa0310@gmail.com>
2025-08-15 21:16:26 +0900

    [level 1] Title: 삼총사, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,11 +1,17 @@
 function solution(number) {
-    var answer = 0;
-    for(let i=0; i<number.length-2; i++){
-        for(let j=i+1; j<number.length-1; j++){
-            for(let k=j+1; k<number.length; k++){
-                if(number[i] + number[j] + number[k] === 0) answer++;
-            }
-        }
+  let result = 0;
+
+  const combination = (current, start) => {
+    if (current.length === 3) {
+      result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+      return;
     }
-    return answer;
+
+    for (let i = start; i < number.length; i++) {
+      combination([...current, number[i]], i + 1);
+    }
+  };
+    
+    combination([],0);
+    return result;
 }
