e72855c02471b2ecd0a16a118de58165bbd0fe0b
Eunso Ahn <eunsoa0310@gmail.com>
2025-08-16 13:43:29 +0900

    [level 2] Title: 짝지어 제거하기, Time: 68.32 ms, Memory: 57.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+  const stack = [];
+  for (let ch of s) {
+    if (stack.length && stack[stack.length - 1] === ch) {
+      stack.pop();
+    } else {
+      stack.push(ch);
+    }
+  }
+  return stack.length === 0 ? 1 : 0;
+}
