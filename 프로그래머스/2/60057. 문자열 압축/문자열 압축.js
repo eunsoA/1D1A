d7808fbdaa01d49d087836b5132f0e03932bab32
Eunso Ahn <eunsoa0310@gmail.com>
2025-09-12 22:24:28 +0900

    [level 2] Title: 문자열 압축, Time: 1.57 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -0,0 +1,24 @@
+function solution(s) {
+  var strLength = s.length;
+
+  // 1부터 s.length /2 까지 split 하고 압축 후 최소 길이 찾기
+  for (let splitLength = 1; splitLength <= s.length / 2; splitLength++) {
+    let splitStr = '';
+    let prvStr = s.slice(0, splitLength);
+    let count = 1;
+    for (let i = splitLength; i < s.length; i += splitLength) {
+      const currentStr = s.slice(i, i + splitLength);
+      if (prvStr === currentStr) {
+        count++;
+      } else {
+        splitStr += (count > 1 ? count : '') + prvStr;
+        prvStr = currentStr;
+        count = 1;
+      }
+    }
+    splitStr += (count > 1 ? count : '') + prvStr;
+
+    strLength = Math.min(strLength, splitStr.length);
+  }
+  return strLength;
+}
