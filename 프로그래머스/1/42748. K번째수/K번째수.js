7293e63e833c7901198f962a5c7056eb0031b897
Eunso Ahn <eunsoa0310@gmail.com>
2025-09-27 22:59:37 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,14 +1,7 @@
-function solve(arr, i, j, k){
-    const tmpArr = arr.slice(i-1,j);
-    tmpArr.sort((a,b)=> a-b);
-    return tmpArr[k-1];
-}
-
 function solution(array, commands) {
-    let answer = [];
-    commands.map((command) => {
-        const tmp = solve(array, command[0], command[1], command[2]);
-        answer.push(tmp);
+    return commands.map((command) => {
+        const [i, j, k] = command;
+        const newArr = array.slice(i-1,j).sort((a,b) => a-b);
+        return newArr[k-1];
     });
-    return answer;
 }
