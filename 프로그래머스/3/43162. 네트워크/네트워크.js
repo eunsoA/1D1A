763e3b576528112a14acb22bd677ef57022fec0e
Eunso Ahn <eunsoa0310@gmail.com>
2025-10-27 11:41:32 +0900

    [level 3] Title: 네트워크, Time: 0.71 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,48 +1,25 @@
+let arr;
+let visitArr;
+
 function solution(n, computers) {
-  const nets = []; // Set들의 배열
-
-  // node가 들어있는 Set의 인덱스 반환, 없으면 -1
-  const findSetIndex = (node) => {
-    for (let k = 0; k < nets.length; k++) {
-      if (nets[k].has(node)) return k;
-    }
-    return -1;
-  };
+  let ctr = 0;
+  arr = computers;
+  visitArr = Array(n).fill(false);
 
   for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      if (computers[i][j] === 1) {
-        const idxI = findSetIndex(i);
-        const idxJ = findSetIndex(j);
+    ctr += dfs(i);
+  }
+  return ctr;
+}
 
-        if (idxI === -1 && idxJ === -1) {
-          // 둘 다 속한 set이 없으면 새로 생성
-          nets.push(new Set([i, j]));
-        } else if (idxI !== -1 && idxJ === -1) {
-          // i가 있는 set에 j 추가
-          nets[idxI].add(j);
-        } else if (idxI === -1 && idxJ !== -1) {
-          // j가 있는 set에 i 추가
-          nets[idxJ].add(i);
-        } else if (idxI !== idxJ) {
-          // 서로 다른 set이면 병합 (작은 인덱스에 합치고 큰 인덱스 삭제)
-          const a = Math.min(idxI, idxJ);
-          const b = Math.max(idxI, idxJ);
-          for (const v of nets[b]) nets[a].add(v);
-          nets.splice(b, 1);
-        }
-        // idxI === idxJ이면 이미 같은 네트워크임
-      }
-      // computers[i][j] === 0일 때는 아무 행동 안 함 (0마다 새 Set 만들면 안 됨)
+function dfs(i) {
+  if (visitArr[i]) return 0;
+  visitArr[i] = true;
+
+  for (let j in arr[i]) {
+    if (arr[i][j] === 1) {
+      dfs(j);
     }
   }
-
-  // 아직 어떤 Set에도 없는(완전 고립된) 노드들을 추가
-  for (let i = 0; i < n; i++) {
-    if (findSetIndex(i) === -1) {
-      nets.push(new Set([i]));
-    }
-  }
-
-  return nets.length;
+  return 1;
 }
