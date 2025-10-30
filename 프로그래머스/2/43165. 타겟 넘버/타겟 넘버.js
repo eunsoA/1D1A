2e08a91d9723042aff65a41e5d84025239aeb146
Eunso Ahn <eunsoa0310@gmail.com>
2025-10-30 16:07:35 +0900

    [level 2] Title: 타겟 넘버, Time: 14.03 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,29 +1,16 @@
 function solution(numbers, target) {
-    numbers.sort();
-    const sum = numbers.reduce((acc,cur) => acc + cur, 0);
-    const mSum = (sum - target)/2;
+    let answer = 0;
+    getAnswer(0,0);
     
-    // numbers에서 각 숫자들은 서로 다른 값일때 mSum 을 만드는 조합의 수.
-    let count = 0;
-    
-    console.log(numbers);
-    
-    const dfs = (index, sum) => {
-        if (index === numbers.length) {
-            return;
+    function getAnswer(index,value) {
+        if(index<numbers.length){
+            getAnswer(index+1,value + numbers[index]);
+            getAnswer(index+1,value - numbers[index]);
+        } else{
+            if(value === target){
+                answer++
+            }
         }
-        
-        sum += numbers[index];
-        
-        if(sum === mSum) {
-            count++;
-        }
-        
-        dfs(index+1, sum);
-        dfs(index+1, sum - numbers[index]);
     }
-    
-    dfs(0,0);
-    
-    return count;
+    return answer;
 }
