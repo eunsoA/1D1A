75fb7bd00d474579ff9c4100ab52749401bd2b83
Eunso Ahn <eunsoa0310@gmail.com>
2025-11-17 10:32:11 +0900

    [level 2] Title: 전화번호 목록, Time: 158.36 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,9 @@
+function solution(phone_book) {
+    phone_book.sort();
+    for(let i = 0; i<phone_book.length -1; i++){
+        const now = phone_book[i];
+        const next = phone_book[i+1];
+        if(next.startsWith(now)) return false;
+    }
+    return true;
+}
